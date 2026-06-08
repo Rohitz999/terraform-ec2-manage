47e66898c27003bc5a4ecd9e61b8b49ff3eece0f
Rohit Kumar Vishwakarma <rohitvishwakarma8082@gmail.com>
2026-06-08 11:42:44 +0000

    update backned.tf

--- backned.tf
+++ backned.tf
@@ -1,7 +1,7 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "terrafrom-state-manage"
-#    key    = "uat/ec2/terraform.tfstate"
-#    region = "ap-south-1"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "terrafrom-state-manage"
+    key    = "uat/ec2/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
